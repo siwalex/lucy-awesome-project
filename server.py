f97a3b0118f1a1de9b12948df44d3eee8c1de82f
Lucaspojo <lucaspaureau@gmail.com>
2020-12-12 00:48:47 +0100

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = os.environ.get('NAME', 'World 5 !!!')
+    name = os.environ.get('NAME', 'World 6 !!!')
     return 'Hello {}!'.format(name)
 
 if __name__ == "__main__":
